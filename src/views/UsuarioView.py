7ab13d2cb5a37172d5b2da601c69532afa7b93ae
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2021-08-07 00:31:29 -0500

    adding delete user, login user

--- src/views/UsuarioView.py
+++ src/views/UsuarioView.py
@@ -6,6 +6,7 @@ from marshmallow import fields,schema,validates,Schema
 from sqlalchemy import Column
 
 from ..models.UserModel import UserModel,Userschema,UserschemaIn
+from ..models.ComprasModel import ComprasModel,Compraschema
 from ..utilities import ReturnCodes
 from ..DBConnector import DBConnector
 import traceback
@@ -13,6 +14,8 @@ import traceback
 db=DBConnector.getDB()
 user_schema =Userschema()
 user_schemaIn = UserschemaIn()
+compras_schema = Compraschema()
+
 
 user_api = Blueprint("user_api",__name__)
 
@@ -91,6 +94,59 @@ def update(id):
         serialized_user = dict()
         serialized_user = user_schema.dump(users)
         return ReturnCodes.custom_response(serialized_user,200,"usuario creado")
+
+       
+    except Exception as ex:
+        return ReturnCodes.custom_response({},409,str(ex))
+
+
+@user_api.route("/<int:id>",methods=["DELETE"])
+def delete(id):
+
+    try:
+        
+        user = UserModel.get_by_id(id)
+        if not user:
+            return ReturnCodes.custom_response({},404,"usuario no encontrado o ya fue borrado")
+        if user.rol ==1:
+            serialized_user = user_schema.dump(user)
+            serialized_user['perfil']=""
+            return ReturnCodes.custom_response(serialized_user,401,"no se debe de borrar el usuario ADMIN")
+
+        #delete compras by producto
+        compra =ComprasModel.del_by_producto(id)
+
+        
+    
+        user.delete()
+        
+        return ReturnCodes.custom_response({"status":"borrado"},200,"usuario borrado")
+
+       
+    except Exception as ex:
+        return ReturnCodes.custom_response({},409,str(ex))
+
+
+
+@user_api.route("/login",methods=["POST"])
+def login():
+
+    try:
+        req_data = request.get_json()
+        data = user_schemaIn.load(req_data)
+        rol = UserModel.get_by_name(req_data['username'])
+
+        if not rol:
+            return ReturnCodes.custom_response({},409,"el usuario no existe")
+
+        if rol.password == req_data['password']:
+            serialized_user = user_schema.dump(rol)
+            return ReturnCodes.custom_response(serialized_user,200,"success")
+        else:
+            serialized_user = user_schema.dump(rol)
+            serialized_user['perfil']=""
+            return ReturnCodes.custom_response({},401,"usuario y/o contraseña incorrecto")
+        
 
        
     except Exception as ex:
